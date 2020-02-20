f8e850c3d6b4cbc2199cb85766c7a468273d5bee
Chandler Van Scoy <chandler@chandlervanscoy.com>
2020-02-20 12:48:36 -0800

    Fix middleware web missing from routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,3 +1,5 @@
 <?php
 
-Route::get(config('bydesign-seamless-auth.auth_route') . '/{guid}', 'DataHead\ByDesignSeamlessAuth\Http\Controllers\ByDesignAuthController@checkGUID');
+Route::middleware('web')->group(function() {
+    Route::get(config('bydesign-seamless-auth.auth_route') . '/{guid}', 'DataHead\ByDesignSeamlessAuth\Http\Controllers\ByDesignAuthController@checkGUID');
+});
